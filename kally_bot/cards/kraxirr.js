383d8e950ff2b0fbeb83d6f04cb55a5dc2db77d5
GlaceJon <nanowrimoijk@gmail.com>
2025-12-15 18:33:43 -0700

    kraxirr power fix

--- kally_bot/cards/kraxirr.js
+++ kally_bot/cards/kraxirr.js
@@ -2,7 +2,7 @@
 module.exports = {
 	name: "Krax'Irr", 
 	faction: "Vel'dhar", 
-	power: 3, 
+	power: 2, 
 	cost: 'KC', 
 	rarity: 'Epic', 
 	text: "Drain 1 life from your opponent and curse his card: Next time you draw this card, will have his text removed.", 
