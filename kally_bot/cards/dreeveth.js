576db32539c196d84aab4d7fcb57a12f3642576a
GlaceJon <nanowrimoijk@gmail.com>
2026-01-23 12:25:17 -0700

    dreeveth typo fix

--- kally_bot/cards/dreeveth.js
+++ kally_bot/cards/dreeveth.js
@@ -5,5 +5,5 @@ module.exports = {
 	power: 2, 
 	cost: 'K', 
 	rarity: 'Rare', 
-	text: "Destroy 2 enemy crystals. If the enemy card was cursed, destroy one more.", 
+	text: "Destroy 2 opponent crystals. If you didn't destroy 2: Curse your opponen't card: The next time you play this card, 2 of your crystals will break.", 
 }
